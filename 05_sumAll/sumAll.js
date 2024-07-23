8d0d61cdfff995c4ee937917ded6c502e71028d9
Alex <alex2323official@gmail.com>
2024-07-23 11:12:23 +0200

    Finished 5th and 6th exercsies with testes

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,45 @@
-const sumAll = function() {
+// Implement a function that takes 2 positive integers and returns the sum of every integer between (and including) them:
 
+// ```javascript
+// sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
+
+const sumAll = function (numberStart, numberEnd) {
+  let numberStartParsed, numberEndParsed;
+
+  // Parsing input: check for negative number, if you find throw ERROR
+  if (
+    numberStart < 0 ||
+    numberEnd < 0 ||
+    numberStart % 1 != 0 ||
+    numberEnd % 1 != 0 ||
+    typeof numberStart !== "number" ||
+    typeof numberEnd !== "number"
+  ) {
+    return "ERROR";
+    // Parsing input: if you don't find negative number go along with code
+  } else {
+    //   Parsing input: check for first big number and assing it to proper variable.
+    if (numberStart > numberEnd) {
+      numberStartParsed = numberEnd;
+      numberEndParsed = numberStart;
+    } else {
+      numberStartParsed = numberStart;
+      numberEndParsed = numberEnd;
+    }
+
+    // Calc start and end
+    let sumAllValue = numberStartParsed + numberEndParsed;
+
+    //   1. Loop trough numbers start and end
+    for (let i = numberStartParsed + 1; i < numberEndParsed; i++) {
+      sumAllValue += i;
+    }
+
+    return sumAllValue;
+  }
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
+// Create a function that determines whether or not a given year is a leap year. Leap years are determined by the following rules:
 
+// > Leap years are years divisible by four (like 1984 and 2004).
+// years divisible by 100 are not leap years (such as 1800 and 1900) unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years).
+
+const leapYears = function (year) {
+  // 1. Check if divisible by 4
+  if (year % 4 == 0) {
+    // Check if divisible by 400 => true
+    if (year % 400 == 0) {
+      return true;
+      // Check if divisible by 400 and by 100 => false
+    } else if (year % 400 !== 0 && year % 100 == 0) {
+      return false;
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
 };
 
+console.log(leapYears(1985));
 // Do not edit below this line
 module.exports = leapYears;
