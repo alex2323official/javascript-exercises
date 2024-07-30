b1dd5595d09cc120d2e788573504252f2a65e36c
Alex <alex2323official@gmail.com>
2024-07-30 18:38:45 +0200

    Finished 9th JS exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
-
+const palindromes = function (stringToCheck) {
+  const tempArray = [];
+  let tempString = "";
+  stringToCheck = stringToCheck.toLowerCase();
+  for (let i = 0; i < stringToCheck.length; i++) {
+    // console.log(stringToCheck[i]);
+    if (
+      stringToCheck[i] != " " &&
+      stringToCheck[i] != "," &&
+      stringToCheck[i] != "-" &&
+      stringToCheck[i] != "!" &&
+      stringToCheck[i] != "."
+    ) {
+      tempArray.push(stringToCheck[i]);
+    }
+  }
+  tempString = tempArray.reduce((accumulator, currentValue) => {
+    return (accumulator += currentValue);
+  });
+  // test if tempString is === to reverse tempString
+  if (tempString == tempString.split("").reverse().join("")) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
