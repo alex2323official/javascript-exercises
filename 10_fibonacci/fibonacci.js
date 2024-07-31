836c11067772d09bacc1c2b12aa49f2c00af6b47
Alex <alex2323official@gmail.com>
2024-07-31 11:16:53 +0200

    Finished 10th exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function (positionToCheck) {
+  if (positionToCheck < 0) {
+    return "OOPS";
+  } else {
+    let f1 = 0;
+    let f2 = 1;
+    let currentFibb = 0;
+    let fibonnaciTable = [];
 
+    // Add fibonacci to array
+    fibonnaciTable.push(f1);
+    fibonnaciTable.push(f2);
+
+    for (let i = 1; i <= positionToCheck - 1; i++) {
+      // Calc current fibb
+      currentFibb = fibonnaciTable[i - 1] + fibonnaciTable[i];
+      fibonnaciTable.push(currentFibb);
+    }
+    return fibonnaciTable[positionToCheck];
+  }
 };
+
+// TESTS:
+console.log(fibonacci(6)); // returns the 4th member of the series: 3  (1, 1, 2, 3)
 
 // Do not edit below this line
 module.exports = fibonacci;
