e009f566c845fa94dead88fa544012ca73088a80
Alex <alex2323official@gmail.com>
2024-07-31 11:39:39 +0200

    Finished 11th JS exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,24 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
 
+const getTheTitles = function (arrayOfBooks) {
+  let value = arrayOfBooks.map((item, i, arr) => {
+    return item.title;
+  });
+  return value;
 };
+
+// Your job is to write a function that takes the array and returns an array of titles:
+
+console.log(getTheTitles(books)); // ['Book','Book2']
 
 // Do not edit below this line
 module.exports = getTheTitles;
